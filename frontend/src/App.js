6b98e539739b91d00e1fbb2ca6143239219d5d41
MatheusOlleNascimento <matheuspontonetrs@gmail.com>
2020-03-26 00:43:28 -0300

    TerceiroCommit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Routes from './routes';
+import './global.css';
+
+//React precisa ser importado sempre que o arquivo trabalhar com JSX
+//JSX:Um HTML está escrito dentro do javascript (Javascript XML)
+
+//Componente: Uma função que retorna HTML podendo ter funcionalidades JS / CSS
+
+//Componente estático não é alterado em nenhuma página:
+//Ex: Header.js = function Header() { return (<header><h1>Texto</h1></header>) ;}
+//Ex: App.js = function App() { return (<Header/>) ;}
+
+//Propriedade de componente com props: (Para o titulo ser dinâmico em cada página):
+//Ex: Header.js = function Header(props) { return (<header><h1>{props.title}</h1></header>) ;}
+//Ex: App.js = function App() { return (<Header title="Semana OmniStack"/>) ;}
+
+//Propriedade de componente com children: (Para o titulo ser dinâmico em cada página):
+//Ex: Header.js = function Header({children}) { return (<header><h1>{children}</h1></header>) ;}
+//Ex: App.js = function App() { return (<Header>Semana OmniStack</Header> ) ;}
+
+//No React não é possível colocar elementos sem ter nada em volta
+//Ex: divs
+
+//No React nunca se deve manupular a variável do estado, precisa-se sobrepor
+//Método useState: retorna um Array com duas posições [valor, funcaoDeAtualizacao]
+//[] em uma variável de armazenamento desestrutura um array
+//Ex: let [counter, setCounter] = useState(0);
+//  setCounter(counter + 1);
+
+//Estado: Sempre que um componente precisa guardar uma informação nele mesmo se aplica o conceito de estado
+//Ex: Através da funcaoDeAtualizacao de uma variável
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello OmniStack
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes />
   );
 }
 
---
+++ frontend/src/routes.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Logon from './pages/Logon';
+import Register from './pages/Register';
+import Profile from './pages/Profile';
+import NewIncident from './pages/NewIncident';
+
+// O Route do react verifica se o caminho começa com o path
+// Usa-se a propriedade exact para o route intender que o path precisa ser exato
+export default function Routes() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={Logon} />
+                <Route path="/register" component={Register} />
+                <Route path="/profile" component={Profile} />
+                <Route path="/incidents/new" component={NewIncident} />
+            </Switch>
+        </BrowserRouter>
+    );
+}
